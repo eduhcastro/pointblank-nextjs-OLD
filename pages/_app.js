6a893c4412df6292ce8e9a794cc89fb661d99a1c
Eduardo <37242504+skillerm@users.noreply.github.com>
2021-01-20 19:55:34 -0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../components/Front/css/home.css'
 import '../components/Front/common.css'
+import '../Accounts.json'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
