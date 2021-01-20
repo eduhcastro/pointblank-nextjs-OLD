5f2f626c192558a6e941f78f4fbb9681fda6fb54
Eduardo <37242504+skillerm@users.noreply.github.com>
2021-01-20 20:20:53 -0300

    Add files via upload

--- pages/api/DatabaseLow.js
+++ pages/api/DatabaseLow.js
@@ -1,6 +1,5 @@
 import low from 'lowdb';
-import  './pages/api/Accounts.json'
-import  'Accounts.json'
+import 'Accounts.json'
 import FileSync from 'lowdb/adapters/FileSync';
 const adapter = new FileSync('./pages/api/Accounts.json')
 const db = low(adapter)
