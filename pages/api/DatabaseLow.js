9b4c11b8f75853fe923f792915fa232b8ed210a9
Eduardo <37242504+skillerm@users.noreply.github.com>
2021-01-21 03:18:59 -0300

    Delete DatabaseLow.js

--- pages/api/DatabaseLow.js
+++
@@ -1,7 +0,0 @@
-import low from 'lowdb';
-import FileSync from 'lowdb/adapters/FileSync';
-const adapter = new FileSync('./path/to/Accounts.json')
-const db = low(adapter)
-db.defaults({ AccountsPLayers: [] }).write();
-
-export default db;
