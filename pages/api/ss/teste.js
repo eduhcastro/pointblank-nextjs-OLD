6b9b7b6432bb034bbaa3d477b172046a462b783b
Eduardo <37242504+skillerm@users.noreply.github.com>
2021-01-21 02:29:28 -0300

    Add files via upload

--- pages/api/ss/teste.js
+++ pages/api/ss/teste.js
@@ -1,9 +1,9 @@
 import consta from '../../../components/Accounts.ts'
 export default async (req, res) => {
-    console.log(consta)
+    console.log(consta[0].name)
     res.status(200).json({
         success: false,
-        data: {},
+        data: {Name: consta[0].name},
         message: 'Not Found'
     });
     res.end();
