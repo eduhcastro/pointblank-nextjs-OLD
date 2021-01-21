1f6b1c5ad2e8aeaa39befaf778d77dab6b121cae
Eduardo <37242504+skillerm@users.noreply.github.com>
2021-01-21 18:37:08 -0300

    Add files via upload

--- public/Base/js/index4.js
+++ public/Base/js/index4.js
@@ -118,3 +118,32 @@ function NameRank(num) {
 }
 
 
+function UserDetail(name){
+	$.get("/api/get/individual", { user: name}, function( rs ){
+			if (rs == null || rs == undefined) {
+				alert("Network error.");
+			} else {
+				if (rs.success == true) {
+					setRankDetailView(true, rs.data, name)
+				}
+				if (rs.message.length > 0) {
+					alert(rs.message);
+				}
+			}		
+})
+
+}
+function setRankDetailView(isView, data, mickname) {
+    if (isView == true) {
+
+        $("#layer_Nickname").text(mickname);
+        $("#layer_UserExp").text(data.Exp ? data.Exp : "");
+        $("#layer_UserRank").text(data.Rank ? data.Rank : "");
+        $("#layer_UserKill").text(data.KillCnt ? data.KillCnt : "");
+        $("#layer_UserDeath").text(data.DeathCnt ? data.DeathCnt : "");
+        $("#layer_UserHeadshot").text(data.HeadShotCnt ? data.HeadShotCnt : "");
+        $("#layer_WinRate").text(data.ClanWinRate ? data.ClanWinRate : "");
+        layer_popup_open_w550('ranking_info');
+    }
+    
+}
